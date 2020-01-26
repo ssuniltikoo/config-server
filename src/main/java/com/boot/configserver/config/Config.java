f0aaaedd613d28c120e4ad63a63d29d22fffdfd5
ssunil.tikoo <ssunil.tikoo@gmail.com>
2020-01-27 00:41:05 +0530

    updated

---
+++ src/main/java/com/boot/configserver/config/Config.java
@@ -0,0 +1,16 @@
+/*
+package com.boot.configserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+
+}
+*/
