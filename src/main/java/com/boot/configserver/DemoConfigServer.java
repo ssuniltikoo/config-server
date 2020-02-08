b8cc0dba312e8f5013ac80bbaa53c242ae00dea6
sunil tikoo <ssunil.tikoo@gmail.com>
2020-02-08 22:17:03 +0530

    updated

--- src/main/java/com/boot/configserver/DemoConfigServer.java
+++ src/main/java/com/boot/configserver/DemoConfigServer.java
@@ -2,9 +2,12 @@ package com.boot.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class DemoConfigServer {
     public static void main(String...args){
